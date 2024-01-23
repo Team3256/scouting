3b49acac698b8aaa1a3c2ee01fc20de2bc39c098
ThatXliner <bryan.hu.2020@gmail.com>
2024-01-22 20:19:46 -0800

    Added tamagui

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { TamaguiProvider } from "tamagui";
+import tamaguiConfig from "@acme/tamagui-config";
 
 import { HeaderBackButton, HeaderTitle } from "../components/header";
 import { TRPCProvider } from "../utils/api";
@@ -10,42 +13,59 @@ import "../styles.css";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { cssInterop } from "nativewind";
+import { useEffect } from "react";
 
 cssInterop(SafeAreaView, { className: "style" });
 
 // This is the main layout of the app
 // It wraps your pages with the providers they need
 export default function RootLayout() {
-  return (
-    <SessionContextProvider supabaseClient={supabase}>
-      <TRPCProvider>
-        {/*
-         * The Stack component displays the current page.
-         * It also allows you to configure your screens
-         */}
-        <Stack
-          screenOptions={{
-            headerLeft: HeaderBackButton,
-            headerTitle: HeaderTitle,
-            headerStyle: {
-              backgroundColor: "#18181A",
-            },
-          }}
-        >
-          {/*
-           * Present the profile screen as a modal
-           * @see https://expo.github.io/router/docs/guides/modals
-           */}
-          <Stack.Screen
-            name="profile"
-            options={{
-              presentation: "modal",
-              headerTitle: () => <></>,
-            }}
-          />
-        </Stack>
-        <StatusBar />
-      </TRPCProvider>
-    </SessionContextProvider>
-  );
+	const [loaded] = useFonts({
+		Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+		InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+	});
+
+	useEffect(() => {
+		if (loaded) {
+			// can hide splash screen here
+		}
+	}, [loaded]);
+
+	if (!loaded) {
+		return null;
+	}
+	return (
+		<TamaguiProvider config={tamaguiConfig}>
+			<SessionContextProvider supabaseClient={supabase}>
+				<TRPCProvider>
+					{/*
+					 * The Stack component displays the current page.
+					 * It also allows you to configure your screens
+					 */}
+					<Stack
+						screenOptions={{
+							headerLeft: HeaderBackButton,
+							headerTitle: HeaderTitle,
+							headerStyle: {
+								backgroundColor: "#18181A",
+							},
+						}}
+					>
+						{/*
+						 * Present the profile screen as a modal
+						 * @see https://expo.github.io/router/docs/guides/modals
+						 */}
+						<Stack.Screen
+							name="profile"
+							options={{
+								presentation: "modal",
+								headerTitle: () => <></>,
+							}}
+						/>
+					</Stack>
+					<StatusBar />
+				</TRPCProvider>
+			</SessionContextProvider>
+		</TamaguiProvider>
+	);
 }
--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import {
-  Alert,
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  Pressable,
-  Text,
-  TextInput,
-  TouchableWithoutFeedback,
-  View,
+	Alert,
+	Keyboard,
+	KeyboardAvoidingView,
+	Platform,
+	Pressable,
+	Text,
+	TextInput,
+	TouchableWithoutFeedback,
+	View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Image } from "expo-image";
@@ -18,154 +18,361 @@ import { FlashList } from "@shopify/flash-list";
 import type { RouterOutputs } from "../utils/api";
 import { AuthAvatar } from "../components/header";
 import { api } from "../utils/api";
+import { quantitativeScouting } from "../../../../packages/db/schema";
+import {
+	Adapt,
+	Button,
+	Input,
+	Label,
+	Select,
+	Sheet,
+	TextArea,
+	XStack,
+	YStack,
+	getFontSize,
+	SelectProps,
+	SizeTokens,
+	FontSizeTokens,
+} from "tamagui";
+import { Check, ChevronDown, ChevronUp } from "@tamagui/lucide-icons";
+import { useMemo, useState } from "react";
+
+const items = [
+	{ name: "Apple" },
+	{ name: "Pear" },
+	{ name: "Blackberry" },
+	{ name: "Peach" },
+	{ name: "Apricot" },
+	{ name: "Melon" },
+	{ name: "Honeydew" },
+	{ name: "Starfruit" },
+	{ name: "Blueberry" },
+	{ name: "Raspberry" },
+	{ name: "Strawberry" },
+	{ name: "Mango" },
+	{ name: "Pineapple" },
+	{ name: "Lime" },
+	{ name: "Lemon" },
+	{ name: "Coconut" },
+	{ name: "Guava" },
+	{ name: "Papaya" },
+	{ name: "Orange" },
+	{ name: "Grape" },
+	{ name: "Jackfruit" },
+	{ name: "Durian" },
+];
+function SelectDemoItem(props: SelectProps) {
+	const [val, setVal] = useState("apple");
+
+	return (
+		<Select
+			id="food"
+			value={val}
+			onValueChange={setVal}
+			disablePreventBodyScroll
+			{...props}
+		>
+			<Select.Trigger width={220} iconAfter={ChevronDown}>
+				<Select.Value placeholder="Something" />
+			</Select.Trigger>
+
+			<Adapt when="sm" platform="touch">
+				<Sheet
+					native={!!props.native}
+					modal
+					dismissOnSnapToBottom
+					animationConfig={{
+						type: "spring",
+						damping: 20,
+						mass: 1.2,
+						stiffness: 250,
+					}}
+				>
+					<Sheet.Frame>
+						<Sheet.ScrollView>
+							<Adapt.Contents />
+						</Sheet.ScrollView>
+					</Sheet.Frame>
+					<Sheet.Overlay
+						animation="lazy"
+						enterStyle={{ opacity: 0 }}
+						exitStyle={{ opacity: 0 }}
+					/>
+				</Sheet>
+			</Adapt>
+
+			<Select.Content zIndex={200000}>
+				<Select.ScrollUpButton
+					alignItems="center"
+					justifyContent="center"
+					position="relative"
+					width="100%"
+					height="$3"
+				>
+					<YStack zIndex={10}>
+						<ChevronUp size={20} />
+					</YStack>
+					{/* <LinearGradient
+						start={[0, 0]}
+						end={[0, 1]}
+						fullscreen
+						colors={["$background", "transparent"]}
+						borderRadius="$4"
+					/> */}
+				</Select.ScrollUpButton>
+
+				<Select.Viewport
+					// to do animations:
+					// animation="quick"
+					// animateOnly={['transform', 'opacity']}
+					// enterStyle={{ o: 0, y: -10 }}
+					// exitStyle={{ o: 0, y: 10 }}
+					minWidth={200}
+				>
+					<Select.Group>
+						<Select.Label>Fruits</Select.Label>
+						{/* for longer lists memoizing these is useful */}
+						{useMemo(
+							() =>
+								items.map((item, i) => {
+									return (
+										<Select.Item
+											index={i}
+											key={item.name}
+											value={item.name.toLowerCase()}
+										>
+											<Select.ItemText>{item.name}</Select.ItemText>
+											<Select.ItemIndicator marginLeft="auto">
+												<Check size={16} />
+											</Select.ItemIndicator>
+										</Select.Item>
+									);
+								}),
+							[items],
+						)}
+					</Select.Group>
+					{/* Native gets an extra icon */}
+					{props.native && (
+						<YStack
+							position="absolute"
+							right={0}
+							top={0}
+							bottom={0}
+							alignItems="center"
+							justifyContent="center"
+							width={"$4"}
+							pointerEvents="none"
+						>
+							<ChevronDown
+								size={getFontSize((props.size as FontSizeTokens) ?? "$true")}
+							/>
+						</YStack>
+					)}
+				</Select.Viewport>
+
+				<Select.ScrollDownButton
+					alignItems="center"
+					justifyContent="center"
+					position="relative"
+					width="100%"
+					height="$3"
+				>
+					<YStack zIndex={10}>
+						<ChevronDown size={20} />
+					</YStack>
+					{/* <LinearGradient
+						start={[0, 0]}
+						end={[0, 1]}
+						fullscreen
+						colors={["transparent", "$background"]}
+						borderRadius="$4"
+					/> */}
+				</Select.ScrollDownButton>
+			</Select.Content>
+		</Select>
+	);
+}
 
 function PostCard(props: { post: RouterOutputs["post"]["all"][number] }) {
-  const { post } = props;
+	const { post } = props;
 
-  const utils = api.useUtils();
+	const utils = api.useUtils();
 
-  const { mutate: deletePost } = api.post.delete.useMutation({
-    onSettled: () => utils.post.all.invalidate(),
-    onError: (error) => {
-      if (error.data?.code === "UNAUTHORIZED")
-        Alert.alert("Error", "Only the author can delete their post");
-    },
-  });
+	const { mutate: deletePost } = api.post.delete.useMutation({
+		onSettled: () => utils.post.all.invalidate(),
+		onError: (error) => {
+			if (error.data?.code === "UNAUTHORIZED")
+				Alert.alert("Error", "Only the author can delete their post");
+		},
+	});
 
-  return (
-    <View className="flex flex-row rounded-lg bg-white/10 p-4">
-      <View className="flex-grow">
-        <Link
-          asChild
-          href={{
-            pathname: "/post/[id]",
-            params: { id: props.post.id },
-          }}
-        >
-          <Pressable>
-            <Image
-              className="mr-2 h-10 w-10 self-center rounded-full"
-              source={post.author?.image ?? ""}
-            />
-            <View>
-              <Text className="text-xl font-semibold text-emerald-400">
-                {post.title}
-              </Text>
-              <Text className="mt-2 text-zinc-200">{post.content}</Text>
-            </View>
-          </Pressable>
-        </Link>
-      </View>
-      <Pressable onPress={() => deletePost(post.id)}>
-        <Text className="font-bold uppercase text-emerald-400">Delete</Text>
-      </Pressable>
-    </View>
-  );
+	return (
+		<View className="flex flex-row rounded-lg bg-white/10 p-4">
+			<View className="flex-grow">
+				<Link
+					asChild
+					href={{
+						pathname: "/post/[id]",
+						params: { id: props.post.id },
+					}}
+				>
+					<Pressable>
+						<Image
+							className="mr-2 h-10 w-10 self-center rounded-full"
+							source={post.author?.image ?? ""}
+						/>
+						<View>
+							<Text className="text-xl font-semibold text-emerald-400">
+								{post.title}
+							</Text>
+							<Text className="mt-2 text-zinc-200">{post.content}</Text>
+						</View>
+					</Pressable>
+				</Link>
+			</View>
+			<Pressable onPress={() => deletePost(post.id)}>
+				<Text className="font-bold uppercase text-emerald-400">Delete</Text>
+			</Pressable>
+		</View>
+	);
 }
 
 function CreatePost() {
-  const utils = api.useUtils();
+	const utils = api.useUtils();
 
-  const [title, setTitle] = React.useState("");
-  const [content, setContent] = React.useState("");
+	const [title, setTitle] = React.useState("");
+	const [content, setContent] = React.useState("");
 
-  const { mutate: createPost, error } = api.post.create.useMutation({
-    onSuccess: async () => {
-      setTitle("");
-      setContent("");
-      Keyboard.dismiss();
-      await utils.post.all.invalidate();
-    },
-    onError: (error) => {
-      if (error.data?.code === "UNAUTHORIZED")
-        Alert.alert("Error", "You must be logged in to create a post");
-    },
-  });
+	const { mutate: createPost, error } = api.post.create.useMutation({
+		onSuccess: async () => {
+			setTitle("");
+			setContent("");
+			Keyboard.dismiss();
+			await utils.post.all.invalidate();
+		},
+		onError: (error) => {
+			if (error.data?.code === "UNAUTHORIZED")
+				Alert.alert("Error", "You must be logged in to create a post");
+		},
+	});
 
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={150}
-    >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss} className="flex-1">
-        <View className="mt-4 justify-around">
-          <TextInput
-            className="mb-2 rounded bg-white/10 p-2 text-zinc-200"
-            placeholderTextColor="#A1A1A9" // zinc-400
-            value={title}
-            onChangeText={setTitle}
-            placeholder="Title"
-          />
-          {error?.data?.zodError?.fieldErrors.title && (
-            <Text className="mb-2 text-red-500">
-              {error.data.zodError.fieldErrors.title}
-            </Text>
-          )}
-          <TextInput
-            className="mb-2 rounded bg-white/10 p-2 text-zinc-200"
-            placeholderTextColor="#A1A1A9" // zinc-400
-            value={content}
-            onChangeText={setContent}
-            placeholder="Content"
-          />
-          {error?.data?.zodError?.fieldErrors.content && (
-            <Text className="mb-2 text-red-500">
-              {error.data.zodError.fieldErrors.content}
-            </Text>
-          )}
-          <Pressable
-            className="rounded bg-emerald-400 p-2"
-            onPress={() => {
-              createPost({
-                title,
-                content,
-              });
-            }}
-          >
-            <Text className="font-semibold text-zinc-900">Publish post</Text>
-          </Pressable>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
-  );
+	return (
+		<KeyboardAvoidingView
+			behavior={Platform.OS === "ios" ? "padding" : "height"}
+			keyboardVerticalOffset={150}
+		>
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss} className="flex-1">
+				<View className="mt-4 justify-around">
+					<TextInput
+						className="mb-2 rounded bg-white/10 p-2 text-zinc-200"
+						placeholderTextColor="#A1A1A9" // zinc-400
+						value={title}
+						onChangeText={setTitle}
+						placeholder="Title"
+					/>
+					{error?.data?.zodError?.fieldErrors.title && (
+						<Text className="mb-2 text-red-500">
+							{error.data.zodError.fieldErrors.title}
+						</Text>
+					)}
+					<TextInput
+						className="mb-2 rounded bg-white/10 p-2 text-zinc-200"
+						placeholderTextColor="#A1A1A9" // zinc-400
+						value={content}
+						onChangeText={setContent}
+						placeholder="Content"
+					/>
+					{error?.data?.zodError?.fieldErrors.content && (
+						<Text className="mb-2 text-red-500">
+							{error.data.zodError.fieldErrors.content}
+						</Text>
+					)}
+					<Pressable
+						className="rounded bg-emerald-400 p-2"
+						onPress={() => {
+							createPost({
+								title,
+								content,
+							});
+						}}
+					>
+						<Text className="font-semibold text-zinc-900">Publish post</Text>
+					</Pressable>
+				</View>
+			</TouchableWithoutFeedback>
+		</KeyboardAvoidingView>
+	);
 }
 
 export default function HomeScreen() {
-  const utils = api.useUtils();
+	const utils = api.useUtils();
 
-  const { data: posts } = api.post.all.useQuery();
+	const { data: posts } = api.post.all.useQuery();
 
-  return (
-    <SafeAreaView className="bg-zinc-900">
-      <Stack.Screen
-        options={{
-          headerLeft: () => <AuthAvatar />,
-          headerTitle: () => (
-            <Text className="text-3xl font-bold text-zinc-200">
-              <Text className="text-fuchsia-500">T3</Text>
-              <Text> x </Text>
-              <Text className="text-emerald-400">Supabase</Text>
-            </Text>
-          ),
-        }}
-      />
-      <View className="h-full w-full p-4">
-        <Pressable
-          className="my-4 rounded bg-emerald-400 p-2"
-          onPress={() => void utils.post.all.invalidate()}
-        >
-          <Text className="font-semibold text-zinc-900">Refresh posts</Text>
-        </Pressable>
+	return (
+		<SafeAreaView className="bg-zinc-900">
+			<Stack.Screen
+				options={{
+					headerLeft: () => <AuthAvatar />,
+					headerTitle: () => (
+						<Text className="text-3xl font-bold text-zinc-200">
+							<Text className="text-fuchsia-500">T3</Text>
+							<Text> x </Text>
+							<Text className="text-emerald-400">Supabase</Text>
+						</Text>
+					),
+				}}
+			/>
+			<View className="h-full w-full p-4">
+				<XStack ai="center">
+					<Label f={1} fb={0} color="white">
+						Native
+					</Label>
 
-        <FlashList
-          data={posts}
-          estimatedItemSize={20}
-          ItemSeparatorComponent={() => <View className="h-2" />}
-          renderItem={(p) => <PostCard post={p.item} />}
-        />
+					<SelectDemoItem native />
+				</XStack>
+				<Pressable
+					className="my-4 rounded bg-emerald-400 p-2"
+					onPress={() => void utils.post.all.invalidate()}
+				>
+					<Text className="font-semibold text-zinc-900">Refresh posts</Text>
+				</Pressable>
 
-        <CreatePost />
-      </View>
-    </SafeAreaView>
-  );
+				<FlashList
+					data={posts}
+					estimatedItemSize={20}
+					ItemSeparatorComponent={() => <View className="h-2" />}
+					renderItem={(p) => <PostCard post={p.item} />}
+				/>
+				<Text>
+					{JSON.stringify(
+						Object.keys(quantitativeScouting).filter(
+							(x) => !(x.toLowerCase().endsWith("id") || x === "createdAt"),
+						),
+					)}
+				</Text>
+				{/* <YStack width={200} minHeight={250} margin="$3" padding="$2">
+					<XStack alignItems="center">
+						<Input flex={1} size={"$2"} placeholder={`Size ${"$2"}...`} />
+						<Button size={"$2"}>Go</Button>
+					</XStack>
+					<XStack alignItems="center">
+						<Input flex={1} size={"$2"} placeholder={`Size ${"$2"}...`} />
+						<Button size={"$2"}>Go</Button>
+					</XStack>
+					<XStack alignItems="center">
+						<Input flex={1} size={"$3"} placeholder={`Size ${"$3"}...`} />
+						<Button size={"$3"}>Go</Button>
+					</XStack>
+					<XStack alignItems="center">
+						<Input flex={1} size={"$4"} placeholder={`Size ${"$4"}...`} />
+						<Button size={"$4"}>Go</Button>
+					</XStack>
+					<TextArea placeholder="Enter your details..." />
+				</YStack> */}
+
+				<CreatePost />
+			</View>
+		</SafeAreaView>
+	);
 }
