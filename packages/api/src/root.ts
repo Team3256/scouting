04288b23e80c62dab6b28015cfe12d2b91460920
ThatXliner <bryan.hu.2020@gmail.com>
2024-02-12 17:39:34 -0800

    Initial implementation of getting match assignments from supabase
    mock data (and mock events) are used. Really bad code. Please clean up for me
    
    Co-Authored-By: Amaar Chughtai <dragonxdev@gmail.com>

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,12 +1,14 @@
 import { attendanceRouter } from "./router/attendance";
 import { authRouter } from "./router/auth";
 import { postRouter } from "./router/post";
+import { scoutingRouter } from "./router/scouting";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   post: postRouter,
   attendance: attendanceRouter,
+  scouting: scoutingRouter,
 });
 
 // export type definition of API
