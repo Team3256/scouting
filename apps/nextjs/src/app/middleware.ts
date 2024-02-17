eec1f61aea485f38de96d08a4b372f5c205b3a70
Amaar Chughtai <amaardevx@gmail.com>
2024-02-17 15:15:42 -0800

    Initial implementation of dboard

--- apps/nextjs/src/app/middleware.ts
+++ apps/nextjs/src/app/middleware.ts
@@ -2,10 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/anyone-can-visit-this-route"],
+  publicRoutes: ["/dashboard"],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ["/no-auth-in-this-route"],
+  ignoredRoutes: ["/dashboard"],
 });
 
 export const config = {
--- apps/nextjs/src/lib/utils/supabase/client.ts
+++ apps/nextjs/src/lib/utils/supabase/client.ts
@@ -1,3 +1,4 @@
+
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
