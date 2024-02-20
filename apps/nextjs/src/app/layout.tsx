b1fb0cadde04d579d67ed80448ec5390bcbd7f58
Amaar Chughtai <amaardevx@gmail.com>
2024-02-20 15:27:35 -0800

    Clerk Authentication stuff

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,8 +1,8 @@
 // import type { Metadata } from "next";
 
 import type { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
-
 // import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
@@ -19,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
