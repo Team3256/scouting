95d4ad738de5cab310e288ae05cb2ee3c560ca26
Bryan Hu <bryan.hu.2020@gmail.com>
2024-02-21 16:10:50 -0800

    Auth actually works now

--- apps/nextjs/src/app/auth/confirm/route.ts
+++ apps/nextjs/src/app/auth/confirm/route.ts
@@ -1,12 +1,9 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
-import { cookies } from "next/headers";
 import { type NextRequest, NextResponse } from "next/server";
 
 import { createClient } from "@/lib/utils/supabase/server";
 
 export async function GET(request: NextRequest) {
-  const cookieStore = cookies();
-
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
@@ -18,7 +15,7 @@ export async function GET(request: NextRequest) {
   redirectTo.searchParams.delete("type");
 
   if (token_hash && type) {
-    const supabase = createClient(cookieStore);
+    const supabase = createClient();
 
     const { error } = await supabase.auth.verifyOtp({
       type,
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,15 +1,18 @@
-"use client";
-
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
-import { SignIn } from "@/components/user-auth-form";
+import { SignUp } from "@/components/user-auth-form";
 import { cn } from "@/lib/utils";
 
-import { signin } from "./actions";
+import { signup } from "./actions";
 
-export default function LoginPage() {
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
+
+export default function AuthenticationPage() {
   return (
     <>
       <div className="md:hidden">
@@ -30,54 +33,53 @@ export default function LoginPage() {
       </div>
       <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-          href="/signup"
+          href="/signin"
           className={cn(
             buttonVariants({ variant: "ghost" }),
             "absolute right-4 top-4 md:right-8 md:top-8",
           )}
         >
-          Sign Up
+          Sign in
         </Link>
         <div className="bg-muted relative hidden h-full flex-col p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           {/* <div className="relative z-20 flex items-center text-lg font-medium">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-6 w-6"
-        >
-          <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-        </svg>
-        Acme Inc
-      </div> */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Acme Inc
+          </div> */}
           {/* <div className="relative z-20 mt-auto">
-        <blockquote className="space-y-2">
-          <p className="text-lg">
-            &ldquo;This library has saved me countless hours of work and
-            helped me deliver stunning designs to my clients faster than
-            ever before.&rdquo;
-          </p>
-          <footer className="text-sm">Sofia Davis</footer>
-        </blockquote>
-      </div> */}
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than
+                ever before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
+          </div> */}
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">Sign In</h1>
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
               <p className="text-muted-foreground text-sm">
-                No account?{" "}
-                <Link className="text-blue-700 underline" href={"/"}>
-                  Sign up.
-                </Link>
+                Enter your email below to create your account
               </p>
             </div>
-            <SignIn action={signin} />
+            <SignUp action={signup} />
             <p className="text-muted-foreground px-8 text-center text-sm">
               By clicking continue, you agree to our{" "}
               <Link
