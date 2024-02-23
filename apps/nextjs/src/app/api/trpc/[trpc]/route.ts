f05cbe58091f409b087c4572df0bb2601558b8b4
Bryan Hu <bryan.hu.2020@gmail.com>
2024-02-22 18:25:50 -0800

    Fix build

--- apps/nextjs/src/app/api/trpc/[trpc]/route.ts
+++ apps/nextjs/src/app/api/trpc/[trpc]/route.ts
@@ -4,14 +4,13 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 import { appRouter, createTRPCContext } from "@acme/api";
-
 // export const runtime = "edge";
 
 /**
  * Configure basic CORS headers
  * You should extend this to match your needs
  */
-function setCorsHeaders(res) {
+function setCorsHeaders(res: Response) {
 	res.headers.set("Access-Control-Allow-Origin", "*");
 	res.headers.set("Access-Control-Request-Method", "*");
 	res.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
