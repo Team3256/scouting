79c8b55d35abcb9d4726d9982db62506db480048
Patrick Woollvin <patrick.woollvin@warriorlife.net>
2024-02-24 13:06:16 -0800

    added stylistic changes

--- apps/nextjs/src/app/signin/page.tsx
+++ apps/nextjs/src/app/signin/page.tsx
@@ -40,7 +40,7 @@ export default function LoginPage() {
         </Link>
         <div className="bg-muted relative hidden h-full flex-col p-10 text-white dark:border-r lg:flex">
           <img src="[]" alt="decorative" />
-          {/* <div className="relative z-20 flex items-center text-lg font-medium">
+          <div className="relative z-20 flex items-center text-lg font-medium">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -54,8 +54,8 @@ export default function LoginPage() {
           <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
         </svg>
         Acme Inc
-      </div> */}
-          {/* <div className="relative z-20 mt-auto">
+      </div>
+          <div className="relative z-20 mt-auto">
         <blockquote className="space-y-2">
           <p className="text-lg">
             &ldquo;This library has saved me countless hours of work and
@@ -64,12 +64,12 @@ export default function LoginPage() {
           </p>
           <footer className="text-sm">Sofia Davis</footer>
         </blockquote>
-      </div> */}
+      </div>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">Sign In</h1>
+              <h1 className="text-2xl font-semibold tracking-tight">Sign In test</h1>
               <p className="text-muted-foreground text-sm">
                 No account?{" "}
                 <Link className="text-blue-700 underline" href={"/"}>
