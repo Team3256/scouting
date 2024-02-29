b18f1502b0c29149acee3f699349311d971a14c4
Amaar Chughtai <amaardevx@gmail.com>
2024-02-28 20:07:10 -0800

    Add functions to add events, add matches, and get emails from the database

--- apps/nextjs/src/app/dashboard/actions.ts
+++ apps/nextjs/src/app/dashboard/actions.ts
@@ -26,3 +26,69 @@ export async function addToDatabase() {
     console.log("Data: ", data);
   }
 }
+
+export async function addEvents({ event }) {
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from("events")
+    .insert([
+      {
+        key: event.key,
+        name: event.name,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error("Error: ", error);
+  } else {
+    console.log("Data: ", data);
+  }
+}
+
+export async function addMatches({ match }) {
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from("matches")
+    .insert([
+      {
+        key: match.match_key,
+        event: match.event,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error("Error: ", error);
+  } else {
+    console.log("Data: ", data);
+  }
+}
+
+export async function getEmails(
+  setMembers: (members: { [key: string]: string[] }) => void,
+) {
+  const supabase = createClient();
+
+  // const { data, error } = await supabase.from("users").select("email");
+  const { data, error } = await supabase.from("profiles").select("email");
+
+  if (error) {
+    console.error("Error: ", error);
+  } else {
+    console.log("Data: ", data);
+  }
+
+  const extractedEmails = data.map((obj) => obj.email);
+
+  // Update the state to create an object where each email is a key with an empty array value
+  setMembers((previousState) => ({
+    ...previousState, // This keeps existing state entries intact
+    ...extractedEmails.reduce((acc, email) => {
+      acc[email] = [];
+      return acc;
+    }, {}),
+  }));
+}
