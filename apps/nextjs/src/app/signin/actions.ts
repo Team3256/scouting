fa96aa80dbd8b635f6fbc926be03333cf3bd0c6a
Bryan Hu <bryan.hu.2020@gmail.com>
2024-03-02 10:33:43 -0800

    fuxed bib fybctuib

--- apps/nextjs/src/app/signin/actions.ts
+++ apps/nextjs/src/app/signin/actions.ts
@@ -6,23 +6,22 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/utils/supabase/server";
 
 export async function signin(formData: FormData) {
-  const supabase = createClient();
+	const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
+	// type-casting here for convenience
+	// in practice, you should validate your inputs
+	const data = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
 
-  const { error } = await supabase.auth.signInWithPassword(data);
-  console.log("error", error);
-  if (error) {
-    // TODO: Form check error
-    redirect("/error");
-  }
+	const { error } = await supabase.auth.signInWithPassword(data);
+	console.log("error", error);
+	if (error) {
+		// TODO: Form check error
+		redirect("/error");
+	}
 
-  revalidatePath("/", "layout");
-  redirect("/dashboard");
+	revalidatePath("/", "layout");
+	redirect("/dashboard");
 }
-
